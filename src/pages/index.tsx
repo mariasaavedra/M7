318d4d1ad0020a80de7962cb93ec37b54b2eb55a
Maria Saavedra <msaav3@gmail.com>
2023-04-30 21:26:48 -0500

    chore: remove some default styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
-import Seo from '@/components/Seo';
-
 /**
  * SVGR Support
  * Caveat: No React Props Type.
@@ -16,14 +13,5 @@ import Seo from '@/components/Seo';
 // to customize the default configuration.
 
 export default function HomePage() {
-  return (
-    <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
-      <main>
-        <section className='bg-white'></section>
-      </main>
-    </Layout>
-  );
+  return <></>;
 }
